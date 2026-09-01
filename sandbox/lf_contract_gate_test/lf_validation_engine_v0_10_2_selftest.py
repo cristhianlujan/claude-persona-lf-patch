47370597fc874dcc87cd001acd85ef9e67a7542f
cristhianlujan <xcrisrhian191080@gmail.com>
2026-09-01 17:29:22 -0500

    test(learning): wire competitive source backlog boundary

--- sandbox/lf_contract_gate_test/lf_validation_engine_v0_10_2_selftest.py
+++ sandbox/lf_contract_gate_test/lf_validation_engine_v0_10_2_selftest.py
@@ -103,6 +103,7 @@ def run_optional_learning_suites()->None:
         "validate_learning_cluster_consumer_coverage_v1.py",
         "validate_learning_next_consumer_applicability_v1.py",
         "validate_learning_additional_consumer_applicability_v1.py",
+        "validate_learning_competitive_source_backlog_readback_v1.py",
         "validate_learning_unbound_cluster_card_readback_v1.py",
         "validate_learning_card_source_boundary_readback_v1.py",
         "validate_learning_exact_nonbinding_guard_v1.py",
