209fe28b98cb3ff0f0cde60007e8989cb71062f8
cristhianlujan <xcrisrhian191080@gmail.com>
2026-09-01 14:35:48 -0500

    test(learning): run unbound-cluster card policy in existing CI entrypoint

--- sandbox/lf_contract_gate_test/lf_validation_engine_v0_10_2_selftest.py
+++ sandbox/lf_contract_gate_test/lf_validation_engine_v0_10_2_selftest.py
@@ -120,6 +120,7 @@ def run_optional_learning_suites() -> None:
         "validate_learning_cluster_consumer_coverage_v1.py",
         "validate_learning_next_consumer_applicability_v1.py",
         "validate_learning_additional_consumer_applicability_v1.py",
+        "validate_learning_unbound_cluster_card_readback_v1.py",
     ]
     executed = 0
     for script in scripts:
