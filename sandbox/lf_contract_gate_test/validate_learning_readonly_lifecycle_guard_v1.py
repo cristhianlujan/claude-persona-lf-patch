708b30264d41d0a70246ff6109bbca8c7161490b
cristhianlujan <xcrisrhian191080@gmail.com>
2026-09-01 16:41:04 -0500

    test(learning): validate technical verification prerequisites

--- sandbox/lf_contract_gate_test/validate_learning_readonly_lifecycle_guard_v1.py
+++ sandbox/lf_contract_gate_test/validate_learning_readonly_lifecycle_guard_v1.py
@@ -10,6 +10,8 @@ req(D['mode']=='READ_ONLY','MODE')
 req(D['behavioral_state']=='INSUFFICIENT_EVIDENCE','BEHAVIORAL_FAIL_CLOSED')
 for state in ('APROBADO','IMPACTADO','VERIFICADO','CERRADO','PRODUCTION_READY','BEHAVIORAL_WINNER'):
     req(state in D['forbidden_without_behavioral_receipt'],'FORBIDDEN_'+state)
+tech=set(D['requirements_for_technical_verification'])
+req(tech=={'CURRENT_HEAD_EXACT_CI_3_OF_3','FRESH_SOURCE_READBACK_FOR_BOUND_EVIDENCE','ACTIVE_BINDING_CONTRACT_COMPLETE','DYNAMIC_EXACT_JOIN_PASS','SELECTOR_ROBUSTNESS_STABILITY_BOUNDEDNESS_PASS','SPECIALIZED_CONSUMERS_FAIL_CLOSED'},'TECHNICAL_REQUIREMENTS')
 req(len(D['requirements_for_behavioral_transition'])==5,'BEHAVIORAL_REQUIREMENTS')
 for c in D['additional_consumers']:
     req(c['state']=='READY_FOR_BINDING_REVIEW','ADDITIONAL_STATE')
@@ -17,4 +19,4 @@ for c in D['additional_consumers']:
     req(c['automatic_binding'] is False,'NO_AUTO_BINDING')
 for k in ('automatic_promotion','automatic_impact','production_authorized','merge_authorized'):
     req(D[k] is False,'AUTH_'+k.upper())
-print('LEARNING_READONLY_LIFECYCLE_GUARD=PASS behavioral=INSUFFICIENT_EVIDENCE additional_consumers=2/2 exact_bindings=0 forbidden_terminal_states=6 production_authorized=false')
+print('LEARNING_READONLY_LIFECYCLE_GUARD=PASS technical_requirements=6 fresh_source_required=true behavioral=INSUFFICIENT_EVIDENCE additional_consumers=2/2 forbidden_terminal_states=6 production_authorized=false')
