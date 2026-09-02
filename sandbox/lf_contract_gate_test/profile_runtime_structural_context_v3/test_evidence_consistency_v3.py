c32f9e0ed04bb520911c85bc5a60fe2a5bcbe076
cristhianlujan <xcrisrhian191080@gmail.com>
2026-09-02 14:56:19 -0500

    test(profile-runtime): align evidence consistency with cleanup gate

--- sandbox/lf_contract_gate_test/profile_runtime_structural_context_v3/test_evidence_consistency_v3.py
+++ sandbox/lf_contract_gate_test/profile_runtime_structural_context_v3/test_evidence_consistency_v3.py
@@ -17,17 +17,22 @@ assert p['artifact_sha256']==ARTIFACT
 q=e['quality_non_regression']
 assert q['run_close_timing_gate']=='14/14'
 assert q['semantic_after_claim_gate']=='8/8'
-assert q['persistent_cpu_boundary_gate']=='13/13'
+assert q['persistent_cpu_boundary_gate']=='18/18'
 assert q['persistent_cpu_adapter_verifier_gate']=='10/10'
-assert q['persistent_cpu_request_wiring_gate']=='8/8'
+assert q['persistent_cpu_request_wiring_gate']=='11/11'
+assert q['persistent_scratch_cleanup_gate']=='PASS'
 assert q['critical_regressions_count']==0
 assert e['semantic_utility']['candidate_after']=='NOT_EXECUTED'
 assert e['semantic_utility']['semantic_after_claim_gate']=='PASS_8_8_FAIL_CLOSED'
 assert e['semantic_utility']['persistent_cpu_runtime']==p['status']=='IMPLEMENTED_DETERMINISTICALLY_TESTED_NO_REAL_INFERENCE'
+assert e['semantic_utility']['persistent_cpu_scope']=='ISOLATED_CANDIDATE_ONLY'
+assert e['semantic_utility']['persistent_cpu_scratch_cleanup']=='REQUIRED_AND_TESTED'
 assert p['real_host_assets_observed'] is False
 assert p['real_host_readiness']=='NOT_EXECUTED'
 assert p['real_model_inference']=='NOT_EXECUTED'
 assert p['semantic_after_status']=='NOT_EXECUTED'
+assert p['implementation']['request_wiring_gate']=='11/11 PASS'
+assert p['implementation']['persistent_scratch_cleanup']=='REQUIRED_AND_TESTED'
 assert e['reporting_governance']['work_report_timing_separated'] is True
 assert e['reporting_governance']['next_execution_readback_required'] is True
 assert c['anti_close_gate']['tests'].startswith('14/14 PASS')
@@ -36,4 +41,4 @@ assert c['semantic_after_gate']['candidate_after_status']=='NOT_EXECUTED'
 assert e['source_evidence']['historical_334_bbox_baseline']=='NOT_PROVEN'
 assert c['historical_334_parity']=='NOT_PROVEN'
 assert c['p02_p03_historical_parity']=='NOT_PROVEN'
-print('PROFILE_RUNTIME_V3_EVIDENCE_CONSISTENCY_PASS 24/24')
+print('PROFILE_RUNTIME_V3_EVIDENCE_CONSISTENCY_PASS 30/30')
