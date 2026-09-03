55b985657ee2cf8ac5bab715625c0471a8cd635d
cristhianlujan <xcrisrhian191080@gmail.com>
2026-09-03 14:23:19 -0500

    test(profile-creator): wire server trust behavioral matrix

--- skills/profile_creator/validators/validate_pack.py
+++ skills/profile_creator/validators/validate_pack.py
@@ -40,6 +40,7 @@ def main():
         ('UPDATE_REVISION_CONTINUITY',[sys.executable,str(root/'evals/update_revision_continuity_contract.py')]),
         ('UPDATE_REVISION_CONTINUITY_MATRIX',[sys.executable,str(root/'evals/update_revision_continuity_matrix.py')]),
         ('UPDATE_SERVER_TRUST_CONTEXT',[sys.executable,str(root/'evals/update_server_trust_context_contract.py')]),
+        ('UPDATE_SERVER_TRUST_BEHAVIOR',[sys.executable,str(root/'evals/update_server_trust_context_behavior.py')]),
     ]
     discovered,discovery_errors=discover_profile_validators(repo_root)
     for slug,validator,profile_dir in discovered: checks.append((f'PROFILE_PACK::{slug}',[sys.executable,str(validator),str(profile_dir)]))
