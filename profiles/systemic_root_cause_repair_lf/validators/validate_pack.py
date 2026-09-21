37eeb13e980b1729350428caa8d2d55a8de21246
cristhianlujan <xcrisrhian191080@gmail.com>
2026-09-21 18:47:55 -0500

    test(srcr): wire V0.5 producer-depth eval into canonical pack validation

--- profiles/systemic_root_cause_repair_lf/validators/validate_pack.py
+++ profiles/systemic_root_cause_repair_lf/validators/validate_pack.py
@@ -12,11 +12,11 @@ REQUIRED = [
     "contracts/evidence_manifest.schema.json","contracts/closure_vocabulary.v2.json","contracts/incremental_value.v1.json",
     "schemas/quality_receipt.schema.json","schemas/runtime_output.schema.json",
     "validators/closure_proof.py","validators/incremental_value.py","validators/runtime_validate.py",
-    "validators/runtime_semantic_utility.py","validators/validate_quality_receipt.py",
+    "validators/runtime_semantic_utility.py","validators/validate_quality_receipt.py","validators/producer_depth.py",
     "evals/v03_contract_schema_cases.py","evals/v03_deterministic_floor_cases.py",
     "evals/v03_quality_receipt_cases.py","evals/v03_generalization_property_cases.py",
     "evals/v03_original_escape_replay.py","evals/v03_transversal_contract_cases.py","evals/v03_incremental_value_cases.py",
-    "evals/v04_transversal_closure_cases.py"
+    "evals/v04_transversal_closure_cases.py","evals/v05_producer_depth_cases.py","contracts/closure_proof_v2.md"
 ]
 
 def fail(code):
@@ -110,6 +110,7 @@ for rel in [
     "evals/v03_transversal_contract_cases.py",
     "evals/v03_incremental_value_cases.py",
     "evals/v04_transversal_closure_cases.py",
+    "evals/v05_producer_depth_cases.py",
     "evals/sandbox_b/run_cases.py",
 ]:
     run_assurance(rel)
