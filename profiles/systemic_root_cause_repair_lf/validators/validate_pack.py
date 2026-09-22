6ca333142588a4f8e76ef5d6c8bbc234dc64292b
cristhianlujan <xcrisrhian191080@gmail.com>
2026-09-22 00:11:39 -0500

    test(srcr): run V0.6 quality receipt materialization cases

--- profiles/systemic_root_cause_repair_lf/validators/validate_pack.py
+++ profiles/systemic_root_cause_repair_lf/validators/validate_pack.py
@@ -125,6 +125,7 @@ for rel in [
     "evals/v05_producer_depth_cases.py",
     "evals/v06_edge_closure_cases.py",
     "evals/v06_control_ownership_cases.py",
+    "evals/v06_quality_receipt_materialization_cases.py",
     "evals/sandbox_b/run_cases.py",
 ]:
     run_assurance(rel)
